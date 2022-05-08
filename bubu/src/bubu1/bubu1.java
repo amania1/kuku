037e316a590fec4c909c03d359c7c9236138fad8
amania1 <amaniaszelalem9@gmail.com>
2022-05-08 17:20:26 -0400

    new chgange

---
+++ bubu/src/bubu1/bubu1.java
@@ -0,0 +1,12 @@
+package bubu1;
+
+public class bubu1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hello world");
+     System.out.println("welcome april class");
+
+	}
+
+}

8a363f093b6a49c097ba28fd4e5b2db445489dcc
amania1 <amaniaszelalem9@gmail.com>
2022-05-08 17:33:07 -0400

    shife made some change

---
+++ shife/src/shife11/shuluka.java
@@ -0,0 +1,12 @@
+package shife11;
+
+public class shuluka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello this is shife ");
+		System.out.println("i made some java change");
+		System.out.println("please let me know if i made a mistake");
+	}
+
+}

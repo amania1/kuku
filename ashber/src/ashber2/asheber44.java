5e6ae2e33e0b1f9c2d6c063b0e3c10b842517973
amania1 <amaniaszelalem9@gmail.com>
2022-05-10 15:32:44 -0400

    new shibe commit

---
+++ ashber/src/ashber2/asheber44.java
@@ -0,0 +1,15 @@
+package ashber2;
+
+public class asheber44 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("hello world 456");
+		 System.out.println("i made amazing changer on this one");
+		 System.out.println("new code avialable");
+		 
+		 
+		 
+	}
+
+}
